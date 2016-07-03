6027cc5de3456d1c3cb6eaff84ef41b35097cf53
yatsuyo <hungry_soul_0122@yahoo.co.jp>
2016-07-03 18:38:07 +0900

    Lesson4_3.4

--- app/src/main/java/com/example/admin/myapp2/MainActivity.java
+++ app/src/main/java/com/example/admin/myapp2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.admin.myapp2;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,13 +23,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
 
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
         mTextView = (TextView) findViewById(R.id.textview);
         mEditText = (EditText) findViewById(R.id.editText);
     }
 
     @Override
-    public void onClick(View v){
-        mTextView.setText(mEditText.getText().toString());
+    public void onClick(View v) {
+        if (v.getId() == R.id.button1) {
+            mTextView.setText(mEditText.getText().toString());
+        } else if (v.getId() == R.id.button2) {
+            showAlertDialog();
+        }
+    }
 
+    private void showAlertDialog() {
+        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("タイトル");
+        alertDialogBuilder.setMessage("メッセージ");
+
+        // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setPositiveButton("肯定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("UI_PARTS", "肯定ボタン");
+                    }
+                });
+
+        // 中立ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setNeutralButton("中立",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("UI_PARTS", "中立ボタン");
+                    }
+                });
+
+        // 否定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setNegativeButton("否定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("UI_PARTS", "否定ボタン");
+                    }
+                });
+
+        // AlertDialogを作成して表示する
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
+
