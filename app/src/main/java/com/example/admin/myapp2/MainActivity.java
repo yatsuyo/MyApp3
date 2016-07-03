037f9694168745e1c13e1e8228fccac428a31fa2
yatsuyo <hungry_soul_0122@yahoo.co.jp>
2016-07-03 15:17:07 +0900

    Lesson4_イベントリスナー

--- app/src/main/java/com/example/admin/myapp2/MainActivity.java
+++ app/src/main/java/com/example/admin/myapp2/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.admin.myapp2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        //button1.setText("ボタン");
+    }
+
+    @Override
+    public void onClick(View v){
+        Log.d("UI_PARTS","ボタンをタップしました");
     }
 }
